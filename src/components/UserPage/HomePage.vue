f43828d8148e322bde8692d8252f8f919c96c690
Jeb <ricojajeb.tayobong@gmail.com>
2025-08-01 22:37:26 +0800

    Install Primevue library

--- src/components/UserPage/HomePage.vue
+++ src/components/UserPage/HomePage.vue
@@ -1,4 +1,6 @@
 <script setup lang="ts">
+import ProgressSpinner from 'primevue/progressspinner'
+
 import { useSystemInfo } from '@/composables/useSystemInfo'
 const { systemInfo, isLoading, error } = useSystemInfo()
 </script>
@@ -6,8 +8,16 @@ const { systemInfo, isLoading, error } = useSystemInfo()
 <template>
   <h1 class="bg-red-700 text-white">Test2 46</h1>
 
-  <div v-if="isLoading">Loading...</div>
-  <div v-else-if="error">Failed to load: {{ error.message }}</div>
+  <div v-if="isLoading">
+    <ProgressSpinner
+      style="width: 50px; height: 50px"
+      strokeWidth="4"
+      fill="transparent"
+      animationDuration="1s"
+      aria-label="Custom ProgressSpinner"
+    />
+  </div>
+  <div v-else-if="error" class="text-red-700">{{ error.message }}</div>
   <div v-else>
     <h1>{{ systemInfo?.system_name }}</h1>
     <p>{{ systemInfo?.system_slogan1 }}</p>
