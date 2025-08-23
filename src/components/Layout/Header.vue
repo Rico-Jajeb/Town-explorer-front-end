b6e045ee14463bceee157848b3198e4a6217bd0b
Jeb <ricojajeb.tayobong@gmail.com>
2025-08-23 21:15:30 +0800

    style: Header modify the authentication

--- src/components/Layout/Header.vue
+++ src/components/Layout/Header.vue
@@ -8,10 +8,16 @@ const { authenticated, user } = useAuthState()
     <RouterLink to="/">Home</RouterLink>
     <RouterLink to="/about">About</RouterLink>
     <RouterLink to="/addData">Add</RouterLink>
-    <RouterLink to="/login">Login</RouterLink>
-    <RouterLink to="/register">Sign Up</RouterLink>
-    <RouterLink to="/logout2">Logout</RouterLink>
-    <RouterLink to="/welcome">welcome user</RouterLink>
+
+    <div v-if="authenticated" class="">
+      <RouterLink to="/logout2">Logout</RouterLink>
+      <RouterLink to="/welcome">welcome user</RouterLink>
+    </div>
+    <div v-else class="">
+      <RouterLink to="/login">Login</RouterLink>
+      <RouterLink to="/register">Sign Up</RouterLink>
+    </div>
+
     <div v-if="authenticated">Hello {{ user.name }}!</div>
     <div v-else class="">guest</div>
   </nav>
