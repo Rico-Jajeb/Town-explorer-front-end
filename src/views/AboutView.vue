a02e2472daa695b941f6f4442b7e33e723fdeed3
Jeb <ricojajeb.tayobong@gmail.com>
2025-09-06 18:55:46 +0800

    TEST 	modified:   src/views/AboutView.vue

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -3,6 +3,7 @@
     <h1>This is an about page</h1>
     <br />
     <h1 class="bg-yellow-300 h-32 p-32">This is an about page</h1>
+    <h1 class="bg-yellow-300 h-32 p-32">This is an about page</h1>
   </div>
 </template>
 
