621cdbbb8790c03e1188ae720718742bda33c868
Jeb <ricojajeb.tayobong@gmail.com>
2025-09-13 07:07:26 +0800

    feat(admin): Add Hamburger button in admin layout

--- src/components/AdminPage/Layout/Sidebar.vue
+++ src/components/AdminPage/Layout/Sidebar.vue
@@ -3,9 +3,9 @@ import { RouterLink } from 'vue-router'
 </script>
 <template>
   <main>
-    <section class="flex flex-col">
+    <section class="flex flex-col fixed top-0 left-0">
       <RouterLink to="/dashboard">Dashboard</RouterLink>
-      <RouterLink to="/dashboard">Dashboard</RouterLink>
+      <RouterLink to="/setting">setting</RouterLink>
       <RouterLink to="/dashboard">Dashboard</RouterLink>
       <RouterLink to="/dashboard">Dashboard</RouterLink>
     </section>
--- src/layout/AdminLayout.vue
+++ src/layout/AdminLayout.vue
@@ -1,4 +1,5 @@
 <script setup>
+import { ref } from 'vue'
 import { RouterLink, RouterView } from 'vue-router'
 import { useToast } from 'primevue/usetoast'
 
@@ -6,13 +7,43 @@ import Sidebar from '@/components/AdminPage/Layout/Sidebar.vue'
 import Header from '@/components/AdminPage/Layout/Header.vue'
 
 const toast = useToast()
+
+const hid = ref(false)
+
+const hamburg = {
+  show() {
+    hid.value = true
+  },
+  hide() {
+    hid.value = false
+  },
+  toggle() {
+    hid.value = !hid.value
+  },
+}
 </script>
 <template>
-  <main class="bg-red-300 flex fle-row">
-    <aside class="basis-1/6 bg-blue-100">
+  <main class="bg-red-300 flex fle-row relative">
+    <aside class="basis-1/8 bg-blue-100" :class="{ hidden: hid }">
       <Sidebar />
     </aside>
-    <section class="basis-5/6 bg-amber-300">
+    <section class="basis-full bg-amber-300 min-h-screen">
+      <button type="button" @click="hamburg.toggle" class="p-4 bg-red-600 text-white rounded">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+          class="w-6 h-6"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="M4 6h16M4 12h16M4 18h16"
+          />
+        </svg>
+      </button>
       <Header />
       <RouterView />
     </section>
